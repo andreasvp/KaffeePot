5f3066095b5263ccbb56a8eb1a405f4635db1a0c
Andreas von Pein <Arbeit@vpein.net>
2020-11-21 12:36:45 +0100

    changes for add kaffee

--- lib/KaffeeCsv/src/KaffeeCsv.cpp
+++ lib/KaffeeCsv/src/KaffeeCsv.cpp
@@ -24,13 +24,13 @@ void createKaffeeCsv()
 	// write file from string stored in flash
 	sdout << F(
 			"3AB59C07,Mitarbeiter1,Qualitätssicherung,10,01:08:14,23.06.16\n"
-			"87594C39,Mitarbeiter2,Entwicklung,10,01:08:14,23.06.16\n"
+			"87594C39,Mitarbeiter2,Entwicklung,40,01:08:14,23.06.16\n"
 			"CAB49116,Mitarbeiter3,Qualitätssicherung,10,01:08:14,23.06.16\n"
-			"296E2887,Mitarbeiter4,Entwicklung,10,01:08:14,23.06.16\n"
-			"FA695AA9,Mitarbeiter5,Entwicklung,10,01:08:14,23.06.16\n"
+			"296E2887,Mitarbeiter4,Entwicklung,40,01:08:14,23.06.16\n"
+			"FA695AA9,Mitarbeiter5,Entwicklung,40,01:08:14,23.06.16\n"
 			"C25846D2,Mitarbeiter6,Qualitätssicherung,10,01:08:14,23.06.16\n"
 			"6D51D0F1,Mitarbeiter7,Qualitätssicherung,10,01:08:14,23.06.16\n"
-			"F7EBF7E5,Mitarbeiter8,Entwicklung,90,01:08:14,23.06.16\n") << flush;
+			"F7EBF7E5,Mitarbeiter8,Entwicklung,40,01:08:14,23.06.16\n") << flush;
 
 	// check for any errors
 	if (!sdout) {
@@ -85,8 +85,96 @@ void listSdFiles(){
 // }
 
 int addKaffeeCsvEntry(String cardId){
- // TODO
- return 1;
+
+// Length (with one extra character for the null terminator)
+int str_len = cardId.length() + 1;
+
+// Prepare the character array (the buffer)
+char char_cardid[str_len];
+
+// Copy it over
+cardId.toCharArray(char_cardid, str_len);
+
+	SFPRINTLN("<Start addKaffeeCsvEntry()>");
+
+	long countCoffeeuser = 0;
+	long totalCoffee = 0;
+
+	// // UID[15] , Name [30], Abteilung[20], KaffeeAnzahl , Zeit[9] , Datum[9]
+	char uidField[15] = {};
+	char nameField[30] = {};
+	char abteilungField[30] = {};
+	long kaffeeAnzahl = 0;
+	char zeitField[9] = {};
+	char datumField[9] = {};
+	char c1 = ' ', c2 = ' ', c3 = ' ', c4 = ' ', c5 = ' ';  // space for commas.
+
+	// open input file
+	ifstream sdin(fileName);
+
+	// check for open error
+	if (!sdin.is_open()) {
+		errorHalt("open");
+	}
+
+	// read until input fails
+	while (1) {
+
+		sdin.get(uidField, sizeof(uidField), ',');
+		if (sdin.fail()) { SFPRINTLN("<dumpBreak uidField>"); break; } // Assume EOF if fail.
+
+	 	sdin >> c1;
+
+		sdin.get(nameField, sizeof(nameField), ',');
+		if (sdin.fail()) { SFPRINTLN("<dumpBreak nameField>"); break; } // Assume EOF if fail.
+
+		sdin >> c2;
+
+		sdin.get(abteilungField, sizeof(abteilungField), ',');
+		if (sdin.fail()) { SFPRINTLN("<dumpBreak abteilungField>"); break; } // Assume EOF if fail.
+
+		sdin >> c3 >> kaffeeAnzahl >> c4;
+
+		// relevante Abteilung
+		if (strcmp(char_cardid,uidField)==0){
+          SFPRINTLN("Found")
+		  Serial.println(nameField);
+
+
+
+            countCoffeeuser = kaffeeAnzahl;
+		}
+		
+		sdin.get(zeitField, sizeof(zeitField), ',');
+		if (sdin.fail()) { SFPRINTLN("<dumpBreak zeitField>"); break; } // Assume EOF if fail.
+
+		sdin >> c5;
+
+		sdin.get(datumField, sizeof(datumField), ',');
+		if (sdin.fail()) { SFPRINTLN("<dumpBreak datumField>"); break; } // Assume EOF if fail.
+
+		//Skip CR/LF.
+		sdin.skipWhite();
+
+		if (sdin.fail()) {
+			errorHalt("bad input");
+		}
+
+		// // error in line if not commas
+		if (c1 != ',' || c2 != ',' || c3 != ',' || c4 != ',' || c5 != ',') {
+			errorHalt("comma");
+		}
+
+	}
+
+	// Error in an input line if file is not at EOF.
+	if (!sdin.eof()) {
+		errorHalt("readFile");
+	}
+
+	SFPRINTLN("<End addKaffeeCsvEntry()>");
+
+	return countCoffeeuser+1;
 }
 
 float avgAbteilung(String abteilung){
@@ -259,8 +347,6 @@ float avgAll(){
 		return totalCoffee/countCoffeeuser;
 	} else return 0;
 }
-
-
 
 void dumpFile() {
 
